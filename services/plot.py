ade19b38ed36fd8bba785a4a00295d4f36fbb0b2
ridiculonius <125882040+ridiculonius@users.noreply.github.com>
2025-08-03 15:03:56 +0300

    Remove cyberpunk style from balance chart

--- services/plot.py
+++ services/plot.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-import mplcyberpunk  # type: ignore
 from matplotlib.dates import DateFormatter
 
 
@@ -13,11 +12,9 @@ def plot_balance_chart(points: List[Tuple[str, float]]) -> BytesIO:
     dates = [datetime.strptime(d, "%Y-%m-%d").date() for d, _ in points]
     balances = [b for _, b in points]
 
-    plt.style.use("cyberpunk")
+    plt.style.use("default")
     fig, ax = plt.subplots(figsize=(8, 4))
-    ax.plot(dates, balances, marker="o", linewidth=2)
-    mplcyberpunk.add_gradient_fill(ax, dates, balances, alpha_fill=0.3)
-    mplcyberpunk.add_glow_effects()
+    ax.plot(dates, balances, color="black", marker="o", linewidth=2)
     ax.set_xlabel("Дата")
     ax.set_ylabel("Баланс, ₽")
     ax.xaxis.set_major_formatter(DateFormatter("%Y-%m-%d"))
